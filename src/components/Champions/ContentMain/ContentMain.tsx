2fcbf6387a35e558a0d88e1a01e2d61cd6d7252c
천정호 <cjsgh85@gmail.com>
2021-04-06 14:04:10 +0900

    [UPDATE] ContentMain 컴포넌트 children 추가
    기존 ContentMain 컴포넌트를 상단과 하단으로 분리하여 자식 컴포넌트를 생성함.
    ChampionListFilter : 상단 컴포넌트, 필터링에 해당하는 기능을 분리
    ChampionList : 하단 컴포넌트, 필터링 된 챔피언 목록을 렌더링하는 컴포넌트

--- src/components/Champions/ContentMain/ContentMain.tsx
+++ src/components/Champions/ContentMain/ContentMain.tsx
@@ -1,41 +1,23 @@
 import React, { useState } from 'react';
 
-import { Container, ChampionListFilter, ChampionSearchInput } from './styles';
+import { Container } from './styles';
 
-const ChampionListFilterItems: [string, string][] = [
-  ['전체', 'ALL'],
-  ['탑', 'TOP'],
-  ['정글', 'JUNGLE'],
-  ['미드', 'MID'],
-  ['바텀', 'BOTTOM'],
-  ['서포터', 'SUPPORTER'],
-  ['로테이션', 'LOTATION'],
-];
+import {ChampionPosition} from '@/components/Champions/championsMeta'
+
+// components
+import ChampionListFilter from './ChampionListFIlter/ChampionListFilter';
+import ChampionList from './ChampionList';
+
+export type filterType = ChampionPosition | 'ALL'
 
 const ContentMain = () => {
-  const [type, setType] = useState<string>('전체');
-  const listItemOnClick = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
-    e.preventDefault();
-    const listType = e.currentTarget.dataset.listType as string;
-    setType(listType);
-  };
-  const createFilterItems = ([key, value]: [string, string]) => {
-    const liClassName = type === value ? 'selected ' : '';
-    const aClassName = value === 'LOTATION' ? 'lotation' : '';
-    return (
-      <li className={liClassName} onClick={listItemOnClick} data-list-type={value} key={key}>
-        <a className={aClassName}>{key}</a>
-      </li>
-    );
-  };
+  const [type, setType] = useState<filterType>('ALL');
+  const [searchText, setSearchText] = useState<string>('');
+  // TODO: Location 챔피언 정보 fetch
   return (
     <Container>
-      <ChampionListFilter>
-        <ul className="champion-list-filter__type">
-          {ChampionListFilterItems.map(createFilterItems)}
-        </ul>
-        <ChampionSearchInput type="text" placeholder="챔피언 검색 (가렌, ㄱㄹ, ...)" />
-      </ChampionListFilter>
+      <ChampionListFilter {...{ type, setType, searchText, setSearchText }} />
+      <ChampionList {...{type}}/>
     </Container>
   );
 };
