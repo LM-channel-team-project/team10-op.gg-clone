4bd1d8ff8fd3c7ae026229dd3a692990037a051a
euntaek <zmn5119@gmail.com>
2021-04-11 19:00:59 +0900

    [ADD] 메인 페이지 광고 배너 컴포넌트 추가

---
+++ src/components/Home/HomeAdsBanner.tsx
@@ -0,0 +1,11 @@
+import S from './style';
+
+function HomeAdsBanner() {
+  return (
+    <S.HomeAdsBannerContainer>
+      <div className="content">광고 배너</div>
+    </S.HomeAdsBannerContainer>
+  );
+}
+
+export default HomeAdsBanner;
---
+++ src/components/Home/style.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 1080px;
+  margin: 0 auto;
+  .main-page-logo {
+    padding: 32px 0 24px;
+  }
+`;
+
+const HomeAdsBannerContainer = styled.aside`
+  display: flex;
+  width: 100%;
+  height: 250px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+
+  .content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 728px;
+    height: 90px;
+    background-color: white;
+    font-size: 1.5rem;
+    font-weight: 600;
+  }
+`;
+
+export default { Container, HomeAdsBannerContainer };
