251ea7fc4429b22e586cd07be77a70f0af625c5d
euntaek <zmn5119@gmail.com>
2021-04-11 18:52:32 +0900

    [UPDATE] 푸터 메타 데이터 이름 변경 및 이미지 주소 추가

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,15 +4,15 @@ import FooterItem from './FooterItem';
 import Icon from '../Common/Icon';
 
 import S from './style';
-import { COPYRIGHT, FOOTER_ADDRESS_LINKS, SNS_ICON_LINKS } from '@/lib/static/footerMeta';
+import { COPYRIGHT, TEAM_ADDRESS_LINKS, SNS_ICON_LINKS } from '@/lib/static/basicMeta';
 
 function Footer({ currentPath }: { currentPath: string }) {
   return (
     <S.FooterContainer currentPath={currentPath}>
       <address className="footer-address">
-        {FOOTER_ADDRESS_LINKS.map((link) => (
-          <FooterItem key={link.name} href={link.href} currentPath={currentPath}>
-            {link.name}
+        {TEAM_ADDRESS_LINKS.map((link) => (
+          <FooterItem key={link.login} href={link.html_url} currentPath={currentPath}>
+            {link.login}
           </FooterItem>
         ))}
       </address>
@@ -20,12 +20,12 @@ function Footer({ currentPath }: { currentPath: string }) {
         <p className="footer-copyright">{COPYRIGHT}</p>
         {SNS_ICON_LINKS.map((icon) => (
           <FooterItem
-            key={icon.name}
-            href={icon.href}
+            key={icon.login}
+            href={icon.html_url}
             currentPath={currentPath}
             className="sns-icon"
           >
-            <Icon name={icon.name} />
+            <Icon name={icon.login} />
           </FooterItem>
         ))}
       </div>
---
+++ src/lib/static/basicMeta.ts
@@ -0,0 +1,50 @@
+export const COPYRIGHT = ` © 2021 OP.GG Clone. OP.GG Cloen isn’t endorsed by Riot Games and doesn’t reflect the views
+or opinions of Riot Games or anyone officially involved in producing or managing League of
+Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot
+Games, Inc. League of Legends © Riot Games, Inc.`;
+
+export const TEAM_ADDRESS_LINKS = [
+  {
+    login: 'About OP.GG Clone',
+    html_url: 'https://github.com/LM-channel-team-project/team-10',
+    avatar_url: '',
+  },
+  {
+    login: 'Leo-Xee',
+    html_url: 'https://github.com/Leo-Xee',
+    avatar_url: 'https://avatars.githubusercontent.com/u/21965795?v=4',
+  },
+  {
+    login: 'Jungho-Cheon',
+    html_url: 'https://github.com/Jungho-Cheon',
+    avatar_url: 'https://avatars.githubusercontent.com/u/61958795?v=4',
+  },
+  {
+    login: '97baek',
+    html_url: 'https://github.com/97baek',
+    avatar_url: 'https://avatars.githubusercontent.com/u/73068489?v=4',
+  },
+  {
+    login: 'euntaek',
+    html_url: 'https://github.com/euntaek',
+    avatar_url: 'https://avatars.githubusercontent.com/u/36879690?v=4',
+  },
+  {
+    login: '라매개발자 채널 팀 프로젝트',
+    html_url: 'https://github.com/LM-channel-team-project',
+    avatar_url: 'https://avatars.githubusercontent.com/u/81451048?v=4',
+  },
+];
+
+export const SNS_ICON_LINKS = [
+  {
+    login: 'github_logo',
+    description: '프로젝트 깃허브 저장소 링크',
+    html_url: 'https://github.com/LM-channel-team-project/team-10',
+  },
+  {
+    login: 'youtube_logo',
+    description: '라매개발자 채널 팀 프로젝트 소개 영상 링크',
+    html_url: 'https://www.youtube.com/watch?v=MiAAodMqO-w',
+  },
+] as const;
