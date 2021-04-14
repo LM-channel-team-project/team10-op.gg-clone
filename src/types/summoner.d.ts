6a601beb2ffea66eefda527e76ad5e36dbcf9d57
euntaek <zmn5119@gmail.com>
2021-04-15 01:00:03 +0900

    [ADD] 소환사 타입 추가

---
+++ src/types/summoner.d.ts
@@ -0,0 +1,31 @@
+export interface BaseSummoner {
+  id: string;
+  accountId: string;
+  puuid: string;
+  name: string;
+  profileIconId: number;
+  revisionDate: number;
+  summonerLevel: number;
+}
+
+export interface BaseLeagueEntry {
+  leagueId: string;
+  queueType: string;
+  tier: string;
+  rank: string;
+  summonerId: string;
+  summonerName: string;
+  leaguePoints: number;
+  wins: number;
+  losses: number;
+  veteran: boolean;
+  inactive: boolean;
+  freshBlood: boolean;
+  hotStreak: boolean;
+}
+
+export interface SummonerRank extends BaseSummoner {
+  tier?: string;
+  rank?: string;
+  leaguePoints?: number;
+}
