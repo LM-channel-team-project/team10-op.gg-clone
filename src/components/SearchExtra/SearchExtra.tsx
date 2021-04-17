a49d7f0a3457696888663a3262af782c64647c95
euntaek <zmn5119@gmail.com>
2021-04-17 17:04:27 +0900

    [ADD] 검색 자동완성 및 히스토리 컨테이너 컴포넌트 추가

---
+++ src/components/SearchExtra/SearchExtra.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import SearchAutoComplete from './SearchAutoComplete';
+import SearchHistory from './SearchHistory';
+import S from './style';
+
+interface SearchExtraProps {
+  open: boolean;
+  value: string;
+  autoCompleteRef: React.Ref<HTMLUListElement>;
+  autoCompleteItemRefs: React.MutableRefObject<HTMLLIElement[]>;
+  autoCompleteFocusIndex: number;
+}
+
+function SearchExtra({
+  open,
+  value,
+  autoCompleteRef,
+  autoCompleteItemRefs,
+  autoCompleteFocusIndex,
+}: SearchExtraProps) {
+  if (!open) return null;
+
+  if (value) {
+    return (
+      <S.SearchExtraContainer>
+        <SearchAutoComplete
+          value={value}
+          autlCompleteRef={autoCompleteRef}
+          autoCompleteItemRefs={autoCompleteItemRefs}
+          autoCompleteFocusIndex={autoCompleteFocusIndex}
+        />
+      </S.SearchExtraContainer>
+    );
+  }
+  return (
+    <S.SearchExtraContainer>
+      <SearchHistory />
+    </S.SearchExtraContainer>
+  );
+}
+
+export default React.memo(SearchExtra);
