d3d782813f628394f40d8ae24e7316b7fd59dc12
euntaek <zmn5119@gmail.com>
2021-06-17 19:48:52 +0900

    [UPDATE] 스크립트 수정 및 환경변수 추가

--- src/lib/api/championInfo.ts
+++ src/lib/api/championInfo.ts
@@ -3,7 +3,7 @@ import client from './client';
 
 export default async function getChampion(championName = '') {
   const championUrl = championName.replace(/(^|\s)\S/g, (char: string) => char.toUpperCase());
-  const apiUrl = `https://ddragon.leagueoflegends.com/cdn/11.7.1/data/ko_KR/champion/${championUrl}.json`;
+  const apiUrl = `${process.env.NEXT_PUBLIC_DDRAGON_URL}/data/ko_KR/champion/${championUrl}.json`;
 
   const response = await client.get<ChampionInfo>(apiUrl);
   const { data } = response.data;
